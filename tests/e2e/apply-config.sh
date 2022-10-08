bed020492204a43033dd5d8b84e0cf790cf41b3b
odacremolbap <pablo@triggermesh.com>
2022-10-08 21:03:03 +0200

    fix1

---
+++ tests/e2e/apply-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p .local/data
+
+cp 01.config.yaml .local/data/config.yaml
---
+++ tests/e2e/start-redis-container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p .local/data
+
+docker run -d -v .local/data:/data \
+    -e REDIS_ARGS="--appendonly yes" \
+    --name redis-stack-server \
+    -p 6379:6379 \
+    redis/redis-stack-server:latest
