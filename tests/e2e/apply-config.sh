0924fe227b0574f98f0010b9508d5f17195a0e33
odacremolbap <pablo@triggermesh.com>
2022-10-08 21:12:43 +0200

    fix1

--- tests/e2e/apply-config.sh
+++ tests/e2e/apply-config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mkdir -p /tests/temp/data
+mkdir -p ./tests/temp/data
 
 ls -la
 ls -la .local
+ls -la .local/data
 
-cp ./tests/e2e/01.config.yaml /tests/temp/data/config.yaml
+cp ./tests/e2e/01.config.yaml ./tests/temp/data/config.yaml
