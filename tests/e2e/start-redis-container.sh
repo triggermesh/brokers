2d1602cd8ed822c40186c28a97f9dd8b775a41cb
odacremolbap <pablo@triggermesh.com>
2022-10-08 21:17:36 +0200

    fix1

--- tests/e2e/start-redis-container.sh
+++ tests/e2e/start-redis-container.sh
@@ -2,7 +2,7 @@
 
 mkdir -p .local/data
 
-docker run -d -v .local/data:/data \
+docker run -d -v ".local/data:/data" \
     -e REDIS_ARGS="--appendonly yes" \
     --name redis-stack-server \
     -p 6379:6379 \
