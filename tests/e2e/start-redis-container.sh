b71bcea3191762bd23872715249361638bfb9a78
odacremolbap <pablo@triggermesh.com>
2022-10-09 09:06:54 +0200

    wait for redis

--- tests/e2e/start-redis-container.sh
+++ tests/e2e/start-redis-container.sh
@@ -8,3 +8,15 @@ docker run -d -v $PWD/.local/data:/data \
     -p 6379:6379 \
     redis/redis-stack-server:latest
 
+for i in {1..5}
+do
+  echo "$i try to connect to Redis."
+  res=`(printf "PING\r\n") | nc localhost 6379 -w1`
+  echo "xx $res xx"
+  if [[ $res == +PONG* ]]; then
+    echo "Redis OK."
+    exit 0
+  fi
+done
+
+exit 6
