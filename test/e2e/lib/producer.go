7d2ccb5ad621dbdd0634a6b1b7e1585a8eb6594d
odacremolbap <pablo@triggermesh.com>
2022-10-12 19:17:25 +0200

    e2e runner framework pt1

--- test/e2e/lib/producer.go
+++ test/e2e/lib/producer.go
@@ -1,5 +1,5 @@
-//go:build e2e
-// +build e2e
+// Copyright 2022 TriggerMesh Inc.
+// SPDX-License-Identifier: Apache-2.0
 
 package lib
 
@@ -9,25 +9,38 @@ import (
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 )
 
-type Producer struct {
-	endpoint string
+type Producer interface {
+	Produce(ctx context.Context, event cloudevents.Event) error
+	GetStoredEvents() []StoredEvent
 }
 
-func NewProducer(endpoint string) *Producer {
-	return &Producer{
+type SimpleProducer struct {
+	endpoint string
+	store    Store
+}
+
+func NewSimpleProducer(endpoint string) Producer {
+	return &SimpleProducer{
 		endpoint: endpoint,
 	}
 }
 
-func (p *Producer) Produce(ctx context.Context, event cloudevents.Event) error {
+func (p *SimpleProducer) Produce(ctx context.Context, event cloudevents.Event) error {
 	c, err := cloudevents.NewClientHTTP()
 	if err != nil {
 		return err
 	}
 
-	if result := c.Send(cloudevents.ContextWithTarget(ctx, p.endpoint), event); cloudevents.IsUndelivered(result) {
+	result := c.Send(cloudevents.ContextWithTarget(ctx, p.endpoint), event)
+
+	p.store.Add(event, StoredEventWithResult(result))
+	if cloudevents.IsUndelivered(result) {
 		return result
 	}
 
 	return nil
+}
+
+func (p *SimpleProducer) GetStoredEvents() []StoredEvent {
+	return p.store.elements
 }
--- test/e2e/redis/redisbroker_test.go
+++ test/e2e/redis/redisbroker_test.go
@@ -1,3 +1,6 @@
+// Copyright 2022 TriggerMesh Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 //go:build e2e
 // +build e2e
 
@@ -5,120 +8,47 @@ package redis
 
 import (
 	"context"
-	"io/ioutil"
-	"os"
-	"sync"
 	"testing"
-	"time"
 
-	"go.uber.org/zap"
-	"go.uber.org/zap/zaptest/observer"
-	"sigs.k8s.io/yaml"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"github.com/triggermesh/brokers/cmd/redis-broker/cmd"
-	pkgcmd "github.com/triggermesh/brokers/pkg/cmd"
+	"github.com/triggermesh/brokers/pkg/backend/impl/redis"
 	"github.com/triggermesh/brokers/pkg/config"
 	"github.com/triggermesh/brokers/test/e2e/lib"
 )
 
-const (
-	initializationWait = time.Second * 2
-	exitTimeout        = time.Second * 5
-)
-
 func TestRedisBroker(t *testing.T) {
-	finished := make(chan error)
-	ctx, cancel := context.WithCancel(context.Background())
-	wg := sync.WaitGroup{}
+	ctx := context.Background()
 
-	cfgfile, err := ioutil.TempFile("", "broker-*.conf")
-	require.NoError(t, err, "Failed to create a temporary configuration file")
-	defer os.Remove(cfgfile.Name())
+	runner := lib.NewBrokerTestRunner(ctx, t)
+	defer runner.CleanUp()
 
-	cfg := config.Config{
+	// TODO customize to use non local brokers, this
+	// args assume a local Redis listening on default port
+	args := &redis.RedisArgs{}
+	backend := redis.New(args, runner.GetGlobals().Logger.Named("redis"))
+	runner.SetupBroker(backend)
+
+	producer := lib.NewSimpleProducer(runner.GetBrokerEndPoint())
+	runner.AddProducer("producer", producer)
+
+	consumer := lib.NewSimpleConsumer(9090)
+	runner.AddConsumer("consumer", consumer)
+
+	cfg := &config.Config{
 		Triggers: map[string]config.Trigger{
 			"test": {
 				Target: config.Target{
-					URL: "http://localhost:9090",
+					URL: consumer.GetConsumerEndPoint(),
 				},
 			},
 		},
 	}
+	runner.UpdateBrokerConfig(cfg)
 
-	b, err := yaml.Marshal(cfg)
-	require.NoError(t, err, "Failed to create configuration for broker")
+	// TODO run schedule
 
-	_, err = cfgfile.Write(b)
-	require.NoError(t, err, "Failed to write configuration file for broker")
+	// logs := runner.GetObservedLogs()
+	// for _, l := range logs.All() {
+	// 	t.Log(l.Message)
 
-	observedZapCore, observedLogs := observer.New(zap.DebugLevel)
-	observedLogger := zap.New(observedZapCore)
-	globals := &pkgcmd.Globals{
-		Logger:     observedLogger.Sugar(),
-		Context:    ctx,
-		ConfigPath: cfgfile.Name(),
-	}
-
-	rb := cmd.StartCmd{}
-
-	go func() {
-		wg.Add(1)
-		finished <- rb.Run(globals)
-		wg.Done()
-	}()
-
-	// Create consumer
-	c, err := lib.NewConsumer(9090)
-	assert.NoError(t, err, "Failed creating consumer")
-
-	go func() {
-		wg.Add(1)
-		err := c.Start(ctx)
-		wg.Done()
-		require.NoError(t, err, "Failed consuming events")
-
-	}()
-
-	select {
-	case <-time.After(initializationWait):
-		// Launch tests
-		p := lib.NewProducer("http://localhost:8080")
-		err = p.Produce(ctx, lib.NewCloudEvent())
-		assert.NoError(t, err, "Failed producing event")
-
-	case err = <-finished:
-		require.NoError(t, err, "Failed running broker server")
-	}
-
-	// Wait for condition based on logs
-	for {
-		// TODO add timeout
-		// TODO add better condition
-		ol := observedLogs.FilterMessageSnippet("Performing")
-		if ol.Len() == 0 {
-			time.Sleep(1 * time.Second)
-			continue
-		}
-		break
-	}
-
-	cancel()
-	if err != nil {
-		return
-	}
-
-	select {
-	case err := <-finished:
-		require.NoError(t, err, "Failed running broker server")
-	case <-time.After(exitTimeout):
-		assert.Fail(t, "Closing the Redis broker timed out")
-	}
-
-	wg.Wait()
-	for i, v := range c.Store.GetAll() {
-		t.Logf("Item received %d: %v", i, v.Event.String())
-	}
-
+	// }
 }
